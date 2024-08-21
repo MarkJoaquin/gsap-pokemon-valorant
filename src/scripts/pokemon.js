7dca44f95100f587524cc488415c9bb6c960ee0e
MarkJoaquin <markjoaquin2@gmail.com>
2024-08-21 02:34:56 -0700

    good advance on pokemon page

--- src/scripts/pokemon.js
+++ src/scripts/pokemon.js
@@ -1,3 +1,53 @@
 $(() => {
-  console.log("pokemon ready");
+  let offset = 0;
+
+  // Fetch data from the PokeAPI
+
+  const fetchPokemonData = () =>
+    $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`,
+      success: (data) => {
+        // console.log(data.results.name[0][1]);
+      },
+    });
+
+  const displayPokemonName = (pokemon) =>
+    $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/`,
+      success: (data) => {
+        // console.log(data);
+        const pokemonCard = $(
+          `<div class="pokemon__card"><p>${data.name}</p></div>`
+        );
+
+        $(".pokemon__container").append(pokemonCard);
+
+        pokemonCard.on("click", () => {
+          displayPokemonImage(pokemon, pokemonCard);
+        });
+      },
+    });
+
+  const displayPokemonImage = (pokemon, pokemonCard) =>
+    $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/`,
+      success: (data) => {
+        // console.log(data);
+
+        pokemonCard.prepend(
+          `<div class="pokemon__image"><img src="${data.sprites.front_default}"></div>`
+        );
+      },
+    });
+
+  $("#fetch").on("click", async () => {
+    const pokemonData = await fetchPokemonData(offset);
+    console.log(pokemonData);
+
+    $.each(pokemonData.results, (index, pokemon) => {
+      // console.log(pokemon.name);
+      displayPokemonName(pokemon.name);
+    });
+    offset += 10;
+  });
 });
---
+++ src/scripts/pokemonGSAP.js
@@ -0,0 +1,4 @@
+gsap.to(".pokemon__card", {
+  duration: 1,
+  stagger: 0.5,
+});
