d7a893f09e274ed5cbbe409492041aaf41bf2790
MarkJoaquin <markjoaquin2@gmail.com>
2024-08-21 16:03:49 -0700

    pokemon page Done

--- src/scripts/pokemon.js
+++ src/scripts/pokemon.js
@@ -9,34 +9,70 @@ $(() => {
       success: (data) => {
         // console.log(data.results.name[0][1]);
       },
-    });
-
-  const displayPokemonName = (pokemon) =>
-    $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/`,
-      success: (data) => {
-        // console.log(data);
-        const pokemonCard = $(
-          `<div class="pokemon__card"><p>${data.name}</p></div>`
-        );
-
-        $(".pokemon__container").append(pokemonCard);
-
-        pokemonCard.on("click", () => {
-          displayPokemonImage(pokemon, pokemonCard);
-        });
+      error: () => {
+        console.log("error fetching data");
       },
     });
 
-  const displayPokemonImage = (pokemon, pokemonCard) =>
+  const displayPokemonName = (pokemonName) =>
     $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/`,
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`,
+      success: (data) => {
+        console.log(data);
+        const pokemonCard = $(
+          `<div class="pokemon__card" id="pokemon__card${data.id}"><p class="pokemon__name">${data.name}</p></div>`
+        );
+
+        $(".pokemon__container").append(pokemonCard);
+        console.log(`#pokemon__card${data.id}`);
+        
+        gsap.from(`#pokemon__card${data.id}`, {
+          opacity: 0,
+          y: -70,
+          duration: 1,
+          stagger: 0.2,
+          ease: "power4.inOut",
+        });
+
+        gsap.to(".pokemon__name", {
+          delay: 1.5,
+          duration: 1.5,
+          y: -20,
+          ease: "back.in(1.7)",
+          scale: 1.2,
+        });
+
+        pokemonCard.on("click", () => {
+          displayPokemonImage(pokemonName, pokemonCard);
+        });
+      },
+      error: () => {
+        console.log("error displaying name");
+      },
+    });
+
+  const displayPokemonImage = (pokemonName, pokemonCard) =>
+    $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`,
       success: (data) => {
         // console.log(data);
 
-        pokemonCard.prepend(
-          `<div class="pokemon__image"><img src="${data.sprites.front_default}"></div>`
-        );
+        pokemonCard
+          .prepend(
+            `<div class="pokemon__image" id="pokemon__image${data.id}"><img src="${data.sprites.front_default}"></div>`
+          )
+          .off();
+
+        gsap.from(`#pokemon__image${data.id}`, {
+          opacity: 0,
+          y: -100,
+          duration: 1,
+          ease: "bounce.out",
+          scale: 15,
+        });
+      },
+      error: () => {
+        console.log("error displaying image");
       },
     });
 
--- src/scripts/pokemonGSAP.js
+++
@@ -1,4 +0,0 @@
-gsap.to(".pokemon__card", {
-  duration: 1,
-  stagger: 0.5,
-});
